41346c3e7a32c9c02db4bb6389585d4bc0f717ba
vrdong <caobadong19@gmail.com>
2018-12-18 16:02:39 +0700

    initial

---
+++ frontend/vue-cli-internet-banking/src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld v-bind:msg="url"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'home',
+  data () {
+    return {
+      url: 'Welcome to the International'
+    }
+  },
+  components: {
+    HelloWorld
+  }
+}
+</script>
